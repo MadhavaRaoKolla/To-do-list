6d7c1ffa5111478dba9faba2f964a3e03457668f
MadhavaRaoKolla <madhavaraokolla2@gmail.com>
2024-06-18 17:44:35 +0530

    Added edit feature

--- app.js
+++ app.js
@@ -1,27 +1,58 @@
 const inputbox = document.getElementById("inputbox");
 const listbox = document.getElementById("listbox");
+const add = document.querySelector('.button');
+let editTask = null;
 
 const addTask = () => {
     if(inputbox.value ===''){
         window.alert("Enter task and then add to list")
     }
+    else if(editTask){
+        editTask.firstChild.textContent=inputbox.value;
+        editTask=null;
+        add.innerHTML="Add";
+    }
     else{
         let li=document.createElement("li");
         li.innerHTML=inputbox.value;
         listbox.appendChild(li);
+
+        let edit = document.createElement('button');
+        edit.innerHTML="Edit";
+        edit.classList.add("edit");
+        li.appendChild(edit);
+
         let close = document.createElement("span");
         close.innerHTML= "\u00d7";
         li.appendChild(close);
     }
     inputbox.value='';
+    saveData();
 }
 
 listbox.addEventListener('click',(e) => {
-    // console.log(e.target.tagName);
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName=='SPAN'){
         e.target.parentElement.remove();
+        saveData();
+    }
+    else if(e.target.tagName=='BUTTON'){
+        inputbox.value=e.target.parentElement.firstChild.textContent;
+        add.innerHTML="Update";
+        editTask = e.target.parentElement;
+        // console.log(editTask);
     }
 });
+
+const saveData = () => {
+    localStorage.setItem("data",listbox.innerHTML)
+}
+
+const showData = () => {
+    listbox.innerHTML = localStorage.getItem("data");
+}
+showData();
+// localStorage.clear();
